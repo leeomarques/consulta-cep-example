f93a82868ec9ef08a7bf164f18a0d9a233428ec3
leeomarques <l30n4rd0@301090>
2023-11-09 22:02:39 -0300

    Projeto em que utilizo RestTemplate consumindo uma API Consulta CEP

--- src/main/java/com/example/consultacepexample/ConsultaCepExampleApplication.java
+++ src/main/java/com/example/consultacepexample/ConsultaCepExampleApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsultaCepExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsultaCepExampleApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultaCepExampleApplication.class, args);
+    }
 
 }
